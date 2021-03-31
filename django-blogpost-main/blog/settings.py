71e0225362d686167da1bbc4d43ff4d227cd013b
Kwongmin Lim <63140834+dnwjd6452@users.noreply.github.com>
2021-03-31 21:48:38 +0900

    Update settings.py

--- django-blogpost-main/blog/settings.py
+++ django-blogpost-main/blog/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,3 +154,8 @@ EMAIL_HOST_PASSWORD = 'bxjwtedoqzpiwril'
 
 
 django_heroku.settings(locals())
+
+
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
