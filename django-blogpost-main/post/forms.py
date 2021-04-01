197a2a2d8483817e0a389b52b5f349ef17865f5c
Kwongmin Lim <63140834+dnwjd6452@users.noreply.github.com>
2021-04-01 17:53:15 +0900

    Update forms.py

--- django-blogpost-main/post/forms.py
+++ django-blogpost-main/post/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Comment
+from .models import Comment, Photo
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'content',) 
+
+class UploadForm(forms.ModelForm):
+    title = forms.CharField(max_length=255)
+    class Meta:
+        model = Photo                         
+        fields = ('image', 'author') 
